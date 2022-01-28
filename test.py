f6c125e74cb87472e9eda4ad104642f740ea4c62
Kevyn Kelso <vyn@Kevyns-MacBook-Pro.local>
2022-01-28 05:45:19 -0700

    fix: show input and output side by side

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
 
-from data import my_data
+from data import IMG_HEIGHT, IMG_WIDTH, my_data
 
 model = tf.keras.models.load_model("my_model")
 train_ds, val_ds = my_data()
@@ -10,7 +10,9 @@ train_ds, val_ds = my_data()
 for input_images, output_images in val_ds.take(1):
     reconst_vec = model.predict(input_images)
     for i in range(25):
+        input_img = input_images[i].numpy()
+        input_img.resize((IMG_WIDTH, IMG_HEIGHT, 3))
         reconst = reconst_vec[i, :, :, :] * 255
         reconst = np.array(reconst)
         reconst = reconst.astype(np.uint8)
-        plt.imsave(f"im{i}.png", np.concatenate((input_images[i], reconst), axis=1)
+        plt.imsave(f"im{i}.png", np.concatenate((input_img, input_img), axis=1))
